a47d66adc706c640f086b6235c523c0d4d824d33
Hùng Nguyễn <vanhung211104@gmail.com>
2025-02-28 08:53:01 +0700

    first commit

---
+++ SpringBoot/src/test/java/ute/nhom27/chatserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ute.nhom27.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
